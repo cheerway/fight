b026ac0679848f36a0c8264f163d8644d78c07e5
cheerway <cheerway@gmail.com>
2015-09-26 22:18:04 +0800

    bootstrap and header

--- app/models/man.rb
+++ app/models/man.rb
@@ -1,3 +1,6 @@
 class Man < ActiveRecord::Base
 	has_many :lists
+	def self.farmers
+		farmers ||=  Man.where("type = 2") 
+	end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root 'farmers#index'
 
+  resources :men
+  resources :products
+  resources :farmers
+  resources :customers
+  resources :comming
+  resources :outting
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
